ff56c7a7a2c06d21758c4387015080932ea33f4d
Seung-Hyun <ee2hi@naver.com>
2026-02-10 17:17:52 +0900

    💬업데이트 내역 수정

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -194,10 +194,8 @@ export default function Sidebar({
               </button>
             </div>
             <ul>
-              <li className="mt-3 text-sm">반응형 UI 수정</li>
-              <li className="mt-3 text-sm">블로그 트리 추가</li>
-              <li className="mt-3 text-sm">TODO 마우스 연동 추가</li>
-              <li className="mt-3 text-sm">웹 아이콘 추가</li>
+              <li className="mt-3 text-sm">고객 센터 답변 여부 배지 추가</li>
+              <li className="mt-3 text-sm">캘린더 / TODO UI 변경</li>
               <li className="mt-3 text-sm">그 외 버그 수정</li>
             </ul>
           </div>
